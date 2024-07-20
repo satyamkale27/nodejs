dce9dd0f2d2319a6e12a5f542474e6c47f957580
satyam <satyamkale271@gmail.com>
2024-07-20 23:43:44 +0530

    sent javascript json

--- webserver/index.js
+++ webserver/index.js
@@ -4,9 +4,15 @@ const { type } = require("os");
 const { text } = require("stream/consumers");
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    "Content-type": "text/plain",
+    // "Content-type": "text/plain",
+    "content-type": "application/json",
   });
-  res.end("Hello! Sir Newton is your friend");
+  res.end(
+    JSON.stringify({
+      id: 1,
+      name: "sir Isaac newton",
+    })
+  );
 });
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
