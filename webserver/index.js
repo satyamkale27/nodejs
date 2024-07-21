98353cf1f8b57fc4f9c8b6e196dfd1a6c74b8498
satyam <satyamkale271@gmail.com>
2024-07-21 17:09:47 +0530

    handling ends points urls

--- webserver/index.js
+++ webserver/index.js
@@ -1,19 +1,40 @@
 const http = require("http");
+const { request } = require("https");
 const PORT = 3000;
 const { type } = require("os");
 const { text } = require("stream/consumers");
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    // "Content-type": "text/plain",
-    "content-type": "application/json",
-  });
-  res.end(
-    JSON.stringify({
-      id: 1,
-      name: "sir Isaac newton",
-    })
-  );
+const server = http.createServer();
+server.on("request", (req, res) => {
+  if (req.url === "/friends") {
+    // res.writeHead(200, {
+    //   // "Content-type": "text/plain",
+    //   "content-type": "application/json",
+    // });
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        id: 1,
+        name: "sir Isaac newton",
+      })
+    );
+  } else if (req.url === "/messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello</li>");
+    res.write("<li>what are your thoughts on astronomy ? </li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
+
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
